a103b2152eaafd43f555432bc9cbd56add693ebd
Arslan Arslan <arslanaarslan@gmail.com>
2024-01-30 00:49:27 +0300

    Add Pizza model and implement CRUD operations
    The Pizza model has been added with its related migrations to establish the database schema. CRUD (Create, Read, Update, Delete) operations on the Pizza objects have also been implemented in the Program.cs file. Additionally, relevant package references for Entity Framework have been added in the project file.

--- PizzaStore/Program.cs
+++ PizzaStore/Program.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.OpenApi.Models;
+using PizzaStore.Models;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("Pizzas") ?? "Data Source=Pizzas.db";
 
 builder.Services.AddEndpointsApiExplorer();
+// builder.Services.AddDbContext<PizzaDB>(options => options.UseInMemoryDatabase("items"));
+builder.Services.AddSqlite<PizzaDB>(connectionString);
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1",
@@ -18,5 +24,39 @@ app.UseSwaggerUI(c =>
 });
 
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/pizzas", async (PizzaDB db) => await db.Pizzas.ToListAsync());
+app.MapPost("/pizza", async (PizzaDB db, Pizza pizza) =>
+{
+    await db.Pizzas.AddAsync(pizza);
+    await db.SaveChangesAsync();
+    return Results.Created($"/pizza/{pizza.Id}", pizza);
+});
+app.MapGet("/pizza/{id}", async (PizzaDB db, int id) => await db.Pizzas.FindAsync(id));
+app.MapPut("/pizza/{id}", async (PizzaDB db, Pizza updatepizza, int id) =>
+{
+    var pizza = await db.Pizzas.FindAsync(id);
+    if (pizza is null) return Results.NotFound();
+    pizza.Name = updatepizza.Name;
+    pizza.Description = updatepizza.Description;
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+});
+app.MapDelete("/pizza/{id}", async (PizzaDB db, int id) =>
+{
+    var pizza = await db.Pizzas.FindAsync(id);
+    if (pizza is null)
+    {
+        return Results.NotFound();
+    }
+    db.Pizzas.Remove(pizza);
+    await db.SaveChangesAsync();
+    return Results.Ok();
+});
+
+// app.MapGet("/pizzas/{id}", (int id) => PizzaDB.GetPizza(id));
+// app.MapGet("/pizzas", () => PizzaDB.GetPizzas());
+// app.MapPost("/pizzas", (Pizza pizza) => PizzaDB.CreatePizza(pizza));
+// app.MapPut("/pizzas", (Pizza pizza) => PizzaDB.UpdatePizza(pizza));
+// app.MapDelete("/pizzas/{id}", (int id) => PizzaDB.RemovePizza(id));
 
 app.Run();
